b23995f03d6e5e60993857c38c182ab04e475df1
Beatriz Rodrigues <beatrizrodrrigues@gmail.com>
2023-08-22 21:43:37 -0300

    exercícios

---
+++ JavaScript para Iniciantes/Tudo é objeto/script-exercicio.js
@@ -0,0 +1,17 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'Beatriz';
+nome.toLocaleLowerCase
+nome.repeat
+nome.length
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn');
+
+btn.classList
+btn.toggleAttribute
+btn.addEventListener
+btn.remove
+btn.animate
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
